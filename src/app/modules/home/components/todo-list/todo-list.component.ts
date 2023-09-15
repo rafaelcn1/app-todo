4c6555d5505620c44dd235ae156ce1afde4397a2
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-15 14:01:28 -0300

    importação do FormsModule

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
-export class TodoListComponent {
-
+export class TodoListComponent implements OnInit {
+  public taskList: Array<TaskList> = [
+    { task: 'Minha primeira tarefa', checked: true },
+    { task: 'Minha segunda tarefa', checked: false },
+  ];
+  ngOnInit(): void {}
 }
