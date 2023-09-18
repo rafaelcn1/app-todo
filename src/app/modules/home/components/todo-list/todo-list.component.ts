b7ea939a49a2aea7fdeaebf9db64c737941095a0
Rafael Cunha <rafaelcn1@hotmail.com>
2023-09-18 15:38:57 -0300

    criação de local Storage

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -8,15 +8,12 @@ import { TaskList } from '../../model/task-list';
 })
 export class TodoListComponent implements OnInit, DoCheck {
   ngDoCheck(): void {
-    // Metodo para ordenação
-    this.taskList.sort(
-      (primeiro, ultimo) => Number(primeiro.checked) - Number(ultimo.checked)
-    ); // Convertendo em numero do primeiro e ultimo checked e fazer o calculo de ordenação, onde todos "checados" vão pra baixo e não checados para cima
+    this.getLocalStorage(); //Chamando o localStorage que tem a lista de tarefa salva
   }
-  public taskList: Array<TaskList> = [
-    //{ task: 'Minha primeira tarefa', checked: true },
-    // { task: 'Minha segunda tarefa', checked: false },
-  ];
+  public taskList: Array<TaskList> = JSON.parse(
+    localStorage.getItem('listaDeTarefas') || //vai buscar um local storage com o nome listaDeTarefas
+      '[]'
+  ); // Ou caso não tenha o  local storage com o nome listaDeTarefas, vai carregar com o array vazio
   ngOnInit(): void {}
 
   /* Metodo para deletar uma tarefa, que recebe um number, que é o index do array taskList */
@@ -48,12 +45,24 @@ export class TodoListComponent implements OnInit, DoCheck {
 
   public validacaoInput(event: String, index: number) {
     const tarefa = this.taskList[index].task;
-    if(!event.length){
-      var confirmacao = window.confirm("Tarefa está vazia, deseja deletar a tarefa?")
-      if(confirmacao) {
+    if (!event.length) {
+      var confirmacao = window.confirm(
+        'Tarefa está vazia, deseja deletar a tarefa?'
+      );
+      if (confirmacao) {
         this.deletarTarefa(index);
       }
     }
+  }
 
+  public getLocalStorage() {
+    if (this.taskList) {
+      // Se o array de tarefas não for vazia
+      // Metodo para ordenação
+      this.taskList.sort(
+        (primeiro, ultimo) => Number(primeiro.checked) - Number(ultimo.checked)
+      ); // Convertendo em numero do primeiro e ultimo checked e fazer o calculo de ordenação, onde todos "checados" vão pra baixo e não checados para cima
+      localStorage.setItem('listaDeTarefas', JSON.stringify(this.taskList)); // Criando um local storage com a key chamada listaDeTarefas e convertendo o array de tarefas em JSON, pois o  localStorage.setItem só aceita String
+    }
   }
 }
